6facbe909c0f0a3377db18c6173bc7ef753fd759
Adam <adam@raspberri.es>
2020-01-14 19:22:57 +0800

    Fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Bms } from "./apis";
 
 const startApp = () => {
   ReactDOM.render(<App />, document.getElementById("root"));
